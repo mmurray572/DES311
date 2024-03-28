00e0b13a09f96a834d76f0981ea2cb52a91454e8
mmurray572 <mmurray572@outlook.com>
2024-03-28 13:05:32 +0000

    did animation code

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
     [SerializeField] private float moveSpeed = 7f;
+
+
+    private bool iswalking;
+
 
     private void Update () {
        Vector2 inputVector = new Vector2(0, 0);
@@ -27,6 +32,12 @@ public class PlayerController : MonoBehaviour
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
 
-        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime);
+        iswalking = moveDir != Vector3.zero;
+        float rotateSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+    }
+
+    public bool IsWalking () {
+        return iswalking;
     }
 }
